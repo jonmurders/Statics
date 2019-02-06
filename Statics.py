4460e08ea0bf408cdc0870ed6c3f6c4cf6ba22a8
Jon Murders <jonmurders@gmail.com>
2019-02-06 09:13:54 -0600

    Starting Here

---
+++ Statics.py
@@ -0,0 +1,9 @@
+#Statics Calculator for common statics problems
+#Jon Murders
+#Originally Written February 6th, 2019
+
+problems = ['Solve for resultant: R','Solve for component: C','Solve for unknown force: F']
+
+for x in problems:
+	print(x)
+
