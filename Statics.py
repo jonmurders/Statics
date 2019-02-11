03cafa5f157514a2b0f46ddf01a32249be4c5702
Jon Murders <jonmurders@gmail.com>
2019-02-11 09:17:09 -0600

    Worked on resultant input functions

--- Statics.py
+++ Statics.py
@@ -7,16 +7,26 @@ import math
 
 #Functions
 def Resultant():
-    number_F = input('How many individual forces do you have?')
-   i=0
+   number_F = input('How many individual forces do you have?')
+   number_F = int(number_F)
+   i=1
    magnitudes = []
    alphas = []
    betas = []
    gammas = []
-   for number_F:
+   while i<=number_F:
        i = i+1
        F_m = input('What is the magnitude of the force?')
-       magnitudes.append(i) = F_m
+       magnitudes.append(F_m)
+       F_alphas = input('What is the alpha direction angle of the force?')
+       alphas.append(F_alphas)
+       F_betas = input('What is the beta direction angle of the force?')
+       betas.append(F_betas)
+       F_gammas = input('What is the gamma direction angle of the force?')
+       gammas.append(F_gammas)
+   
+
+
 
 
 
@@ -28,14 +38,14 @@ for x in problems:
 	print(x)
 choice1 = input('What would you like to do?')
 
-if choice1 = 'R':
-    print('Working on')
+if choice1 == 'R':
+    Resultant()
 
 
-if choice1 = 'C':
+if choice1 == 'C':
     print('Sorry this is not supported yet')
 
-if choice1 = 'F':
+if choice1 == 'F':
     print('Sorry this is not supported yet')
 
 else:
